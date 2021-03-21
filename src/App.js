cdc276b0883c4ead1f7185e9707b4b6781094f31
Jonah Fleishhacker <jfleishhacker@uchicago.edu>
2021-03-21 02:24:18 -0500

    Can lookup BOMs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import MaterialTable from 'material-table'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
 
   const [data, setData] = useState([])
+  const [selectedBom, setBom] = useState("")
+
   const columns = [
     { title: "ID", field: "id"},
     { title: "bomId", field: "bomId"},
@@ -17,16 +18,49 @@ function App() {
     { title: "item_unit_cost", field: "item_unit_cost"},
     { title: "specific_part", field: "specific_part"}
   ]
-  
-  useEffect(() => {
-    fetch("https://5f996efe50d84900163b8a42.mockapi.io/api/v1/bom/1001/bomitem")
+
+  function fetchBOM(e) {
+    e.preventDefault()
+    fetch("https://5f996efe50d84900163b8a42.mockapi.io/api/v1/bom/" + selectedBom + "/bomitem")
     .then(resp=>resp.json())
     .then(resp=>setData(resp))
-  }, [])
+  }
+
+  var formStyle = {
+      padding: 10,
+      margin: 10,
+      backgroundColor: "#fffccc",
+      color: "#333",
+      display: "inline-block",
+      fontFamily: "monospace",
+      fontSize: 16,
+      textAlign: "center"
+  };
+
+  var divStyle = {
+    margin: "auto",
+    width: "50%",
+    padding: 10
+  };
 
   return (
     <div className="App">
-      <h4 align="center"> Bill of materials</h4>
+      <div style={divStyle}>
+      <form onSubmit = {fetchBOM}>
+        <label>
+          Enter Bom ID:
+          <input style={formStyle}
+            type = "text" 
+            name = "bom ID" 
+            value = {selectedBom} 
+            onChange = {(e) => setBom(e.target.value)}
+            />
+
+        </label>
+        <input type="submit" value="Submit"  />
+      </form>
+      </div>
+
       <MaterialTable
         title = "Bill of materials"
         data = {data}
